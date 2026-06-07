ddcecc64c2735f85d0ab54169602dfe1b1c2a5e6
brandon-300 <g09649009@gmail.com>
2026-06-07 23:15:19 +0100

    Delete chat directory

--- chat/js/config.js
+++
@@ -1,2 +0,0 @@
-export const SUPABASE_URL = "https://yphxpgssdqboufbgazwi.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_eKe5liyr5Mc-mumZbXLuZw_Pt3-KVm0";
--- chat/js/supabase.js
+++
@@ -1,2 +0,0 @@
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./config.js";
-export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
